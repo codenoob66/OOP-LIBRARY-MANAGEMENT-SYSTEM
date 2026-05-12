38c0a58f43d0e6be87213e4c7aaa71949013e9c1
rafael vincent <d749107@gmail.com>
2026-05-12 08:52:16 +0800

    worked on borrow service

---
+++ classes/BorrowService.php
@@ -0,0 +1,33 @@
+<?php
+    class BorrowService
+    {
+        private int $allowedBooksToBorrow = 5;
+
+        public function borrowBook(User $user, Book $book)
+        {
+            $bookCount = count($user->getBorrowedBooks());
+            $books_Array = $user->getBorrowedBooks();
+
+            if($bookCount >= $this->allowedBooksToBorrow) {
+                return false;
+            }
+            if(in_array($book, $books_Array)) {
+                return false;
+            } else {
+                if($book->borrowBook() == true) {
+                   $user->addBorrowedBook($book);
+                   return true;
+                }
+            }
+        }
+
+        public function returnBook(User $user, Book $book)
+        {
+            if($user->returnBook($book)) {
+                $book->returnBook();
+                return true;
+            }
+                return false;
+        }
+    }
+?>
--- classes/User.php
+++ classes/User.php
@@ -4,7 +4,6 @@
         private int $id;
         private string $name;
         private array $borrowedBooks = [];
-        private int $allowedBooksToBorrow = 5;
 
 
         public function __construct(int $id, string $name)   
@@ -25,41 +24,41 @@
         }
 
 
-        public function returnBook(Book $book)
-        {
+        // public function returnBook(Book $book)
+        // {
            
-            if(in_array($book, $this->borrowedBooks)) {
-                if (($key = array_search($book, $this->borrowedBooks)) !== false) {
-                unset($this->borrowedBooks[$key]);
-                $this->borrowedBooks = array_values($this->borrowedBooks);
-                 $book->returnBook();
-                 return true;
-                }
-            }
-                return false;
-        }
-
-        public function borrowBook(Book $book)
-        {
-
-            if(count($this->borrowedBooks) >= $this->allowedBooksToBorrow) {
-                return false;
-            }
-            if(in_array($book, $this->borrowedBooks)) {
-                return false;
-            } else {
-                if($book->borrowBook() == true){
-                array_push($this->borrowedBooks, $book);
-                return true;
-                }
-                return false;
-            }
-        }
+        //     if(in_array($book, $this->borrowedBooks)) {
+        //         if (($key = array_search($book, $this->borrowedBooks)) !== false) {
+        //         unset($this->borrowedBooks[$key]);
+        //         $this->borrowedBooks = array_values($this->borrowedBooks);
+        //          $book->returnBook();
+        //          return true;
+        //         }
+        //     }
+        //         return false;
+        // }
 
         public function getBorrowedBooks(): array
         {
-            
             return $this->borrowedBooks;
+        }
+
+        public function addBorrowedBook(Book $book)
+        {
+            return $this->borrowedBooks[] = $book;
+        }
+
+        public function returnBook(Book $book)
+        {
+            if(in_array($book, $this->borrowedBooks)){
+
+                if (($key = array_search($book, $this->borrowedBooks)) !== false) {
+                unset($this->borrowedBooks[$key]);
+                $this->borrowedBooks = array_values($this->borrowedBooks);
+                return true;
+
+                }
+            }
         }
     }
 ?>
