e3b51d291239547d5cf327eeb75b45f70860e881
rafael vincent <d749107@gmail.com>
2026-05-13 20:10:22 +0800

    working

--- database/Database.php
+++ database/Database.php
@@ -8,6 +8,7 @@
         private $password;
         private $obj;
         private static $connection = null;
+        
 
 
         private function __construct()
@@ -23,10 +24,12 @@
                 if(self::$connection === null) {
                 $dsn = "mysql:host=" . $config['host'] . ";dbname=" . $config['dbname'] . ";charset=utf8mb4";
                 self::$connection = new PDO($dsn, $config['username'], $config['password']);
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
                 return self::$connection;
             } catch(PDOException $error) {
                 die("Connection failed: " . $error->getMessage());
+                
             }
         }
     }
