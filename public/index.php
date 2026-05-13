ef4b70efa272bf45d90f4e5b4f5966885b08c7eb
rafael vincent <d749107@gmail.com>
2026-05-13 20:10:40 +0800

    all

--- public/index.php
+++ public/index.php
@@ -1,19 +1,8 @@
 <?php
-
-
     spl_autoload_register(function ($className) {
     $directories = ['classes', 'database'];
 
-    // foreach($directories as $dir) {
-    //     $filepath = __DIR__ . '/../' . $dir . '/' . $className . '.php';
-        
-    //     if(file_exists($filepath)) {
-    //         include $filepath;
-    //         return;
-    //     }
-    // }
-
-    for($i = 0; $i <= count($directories); $i++) {
+    for($i = 0; $i <  count($directories); $i++) {
         $filepath = __DIR__ . '/../' . $directories[$i] . '/' . $className . '.php';
 
         if(file_exists($filepath)) {
@@ -24,12 +13,9 @@
     }
     });
 
-    $db1 = Database::getConnection();
-    $db2 = Database::getConnection();
+    $user = new User('1', "rafael");
 
-    if ($db1 === $db2) {
-    echo "Singleton works!";
-} else {
-    echo "Singleton failed!";
-}
+    echo $user->getId();
+
+
 ?>
